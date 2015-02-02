5c37669203eb0085dbe134127151c1bd60cf1809
Helena V. Icart <helenav.icart@gmail.com>
2015-02-02 15:24:46 +0100

    New changes to Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,10 @@ class Project < ActiveRecord::Base
 
   def self.iron_find(n)
     where("name = ?", n)
-
   end
+
+  def self.clean_old
+    where("created_at < ?", 7.day.ago).destroy_all
+  end
+
 end
